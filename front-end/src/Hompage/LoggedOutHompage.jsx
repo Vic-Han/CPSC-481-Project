1dc906957cfbaa941ce340a6bcfed349a3af132f
Saksham Puri <84263133+Saksham-P@users.noreply.github.com>
2023-11-05 13:26:29 -0700

    Finalized Logged Out Designs
    Added Custom Scrollbar, Added Parallax Effect, Create an Account button leads to correct overlay.

--- front-end/src/Hompage/LoggedOutHompage.jsx
+++ front-end/src/Hompage/LoggedOutHompage.jsx
@@ -1,32 +1,42 @@
 import './LoggedOutHomepage.css';
 import './Circles.css';
-import {React, useState} from "react";
+import {React, useEffect, useState} from "react";
 import example1 from '../assets/example1.png';
 import example2 from '../assets/example2.png';
 
-function LoggedOutHomepage() {
+function LoggedOutHomepage(props) {
+  const registerEvent = props.registerEvent2;
+
+  const [offsetY, setOffsetY] = useState(0);
+  const handleScroll = () => setOffsetY(window.scrollY);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  });
+
   return (
     <div className='main_content'>
       <div>
-        <div className="circle" id="circle1"></div>
-        <div className="circle" id="circle2"></div>
-        <div className="circle" id="circle3"></div>
-        <div className="circle" id="circle4"></div>
-        <div className="circle" id="circle5"></div>
-        <div className="circle" id="circle6"></div>
-        <div className="circle" id="circle7"></div>
-        <div className="circle" id="circle8"></div>
-        <div className="circle" id="circle9"></div>
-        <div className="circle" id="circle10"></div>
-        <div className="circle" id="circle11"></div>
-        <div className="circle" id="circle12"></div>
-        <div className="circle" id="circle13"></div>
-        <div className="circle" id="circle14"></div>
-        <div className="circle" id="circle15"></div>
-        <div className="circle" id="circle16"></div>
-        <div className='circle' id='image1'></div>
-        <div className='circle' id='image2'></div>
-        <div className='circle' id='image3'></div>
+        <div className="circle" id="circle1" style={{transform: `translateY(${offsetY * 0.20}px)`}}></div>
+        <div className="circle" id="circle2" style={{transform: `translateY(${offsetY * 0.17}px)`}}></div>
+        <div className="circle" id="circle3" style={{transform: `translateY(${offsetY * 0.23}px)`}}></div>
+        <div className="circle" id="circle4" style={{transform: `translateY(${offsetY * 0.18}px)`}}></div>
+        <div className="circle" id="circle5" style={{transform: `translateY(${offsetY * 0.22}px)`}}></div>
+        <div className="circle" id="circle6" style={{transform: `translateY(${offsetY * 0.19}px)`}}></div>
+        <div className="circle" id="circle7" style={{transform: `translateY(${offsetY * -0.073}px)`}}></div>
+        <div className="circle" id="circle8" style={{transform: `translateY(${offsetY * -0.065}px)`}}></div>
+        <div className="circle" id="circle9" style={{transform: `translateY(${offsetY * -0.05}px)`}}></div>
+        <div className="circle" id="circle10" style={{transform: `translateY(${offsetY * -0.045}px)`}}></div>
+        <div className="circle" id="circle11" style={{transform: `translateY(${offsetY * -0.03}px)`}}></div>
+        <div className="circle" id="circle12" style={{transform: `translateY(${offsetY * -0.07}px)`}}></div>
+        <div className="circle" id="circle13" style={{transform: `translateY(${offsetY * -0.06}px)`}}></div>
+        <div className="circle" id="circle14" style={{transform: `translateY(${offsetY * -0.055}px)`}}></div>
+        <div className="circle" id="circle15" style={{transform: `translateY(${offsetY * -0.04}px)`}}></div>
+        <div className="circle" id="circle16" style={{transform: `translateY(${offsetY * -0.03}px)`}}></div>
+        <div className='circle' id='image1' style={{transform: `translateY(${offsetY * 0.1}px)`}}></div>
+        <div className='circle' id='image2' style={{transform: `translateY(${offsetY * 0.1}px)`}}></div>
+        <div className='circle' id='image3' style={{transform: `translateY(${offsetY * 0.1}px)`}}></div>
       </div>
       <div className='text_container1'>
         <h1 className='intro_title'>Welcome to the <span className='highlighted'>Ultimate</span> Gaming Experience!</h1>
@@ -35,7 +45,7 @@ function LoggedOutHomepage() {
       </div>
       <div className='text_container2'>
         <h1 className='intro_title'>Don't want to miss all the <span className='highlighted'>ACTION</span>?</h1>
-        <p className='intro_text'><a href='#' className='link'>Create an Account</a> and dive right in and explore the fun.</p>
+        <p className='intro_text'><a className='link' onClick={registerEvent}>Create an Account</a> and dive right in and explore the fun.</p>
       </div>
       <img className='example' id='example1' src={example1}></img>
       <img className='example' id='example2' src={example2}></img>
