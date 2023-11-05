005afa41a17feb51ee5931ea9bb48f2a6fe76e4c
Saksham Puri <84263133+Saksham-P@users.noreply.github.com>
2023-11-04 20:39:46 -0600

    Created Logged Out Navbar

--- front-end/src/Navbar/LoggedOutNavbar.jsx
+++ front-end/src/Navbar/LoggedOutNavbar.jsx
@@ -1,15 +1,24 @@
 import './LoggedOutNavbar.css';
+import logo from '../assets/EZLogo-v2.png'
 import {React, useState} from "react"
+
 function LoggedOutNavbar(props) {
-    const registerEvent = props.registerEvent
-    const loginEvent = props.loginEvent     
+  const registerEvent = props.registerEvent;
+  const loginEvent = props.loginEvent;
+
   return (
     <div className='navbar_container_logged_out'>
-        <img src= "" alt = "logo" className=''/>
-        <div>Esports Zone</div>
-        <div>About Us</div>
-        <div onClick={registerEvent}>Sign Up</div>
-        <button onClick={loginEvent}>Login</button>
+      <div className='navbar_left_container'>
+        <img src={logo}></img>
+        <div className='navbar_title'>ESports Zone</div>
+      </div>
+      <div className='navbar_right_container'>
+        <div className='navbar_about'>About Us</div>
+        <div className='navbar_register' onClick={registerEvent}>Sign Up</div>
+        <div className='navbar_login_container' onClick={loginEvent}>
+          <div className='navbar_login'>Login</div>
+        </div>
+      </div>
     </div>
   );
 }
