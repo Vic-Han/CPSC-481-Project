70ccb7939bf1d30dcf224c316d5243870eb9a97b
Saksham Puri <84263133+Saksham-P@users.noreply.github.com>
2023-11-06 00:09:14 -0700

    Completed Navbar Logged Out & In
    Filled all buttons with links, and made all of them focusable using tab by converting them to buttons.  Removed unnecessary Imports from files.

--- front-end/src/Navbar/Navbar.jsx
+++ front-end/src/Navbar/Navbar.jsx
@@ -1,10 +1,4 @@
 import './Navbar.css';
-import logo from '../assets/EZLogo-v2.png';
-import GameStore from '../assets/GameStore.png';
-import DM from '../assets/DM.png';
-import ProfileDefault from '../assets/ProfileDefault.png';
-import Search from '../assets/Search.png';
-import {React, useState} from "react";
 
 function Navbar(props) {
   const toggleHomePage = props.clickHandlers.toggleHomePage
@@ -16,19 +10,19 @@ function Navbar(props) {
   return (
     <div className='navbar_container'>
       <div className='navbar_left'>
-        <img src={logo}></img>
-        <button className='navbar_post_container'>
+        <button className='navbar_logo' onClick={toggleHomePage}></button>
+        <button className='navbar_post_container' onClick={toggleNewPost}>
           <div className='navbar_post'>New Post</div>
         </button>
       </div>
       <div className='navbar_middle'>
         <input type='text' placeholder='Search'></input>
-        <img src={Search} tabIndex={0}></img>
+        <button className='navbar_search btn' onClick={toggleSearchPage}></button>
       </div>
       <div className='navbar_right'>
-        <img src={GameStore}></img>
-        <img src={DM}></img>
-        <img src={ProfileDefault}></img>
+        <button className='navbar_game btn' onClick={toggleGameStore}></button>
+        <button className='navbar_dm btn' onClick={toggleDM}></button>
+        <button className='navbar_profile btn' onClick={toggleGameStore}></button>
       </div>
     </div>
   );
