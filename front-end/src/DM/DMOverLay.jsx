1c7eea5c5d59dd787015567d784bb409495e1158
MohammedH41 <122569808+MohammedH41@users.noreply.github.com>
2023-11-14 19:03:26 -0700

    Started styling Dm overlay

--- front-end/src/DM/DMOverLay.jsx
+++ front-end/src/DM/DMOverLay.jsx
@@ -1,6 +1,6 @@
 import './DMOverLay.css';
-import {React, useState} from "react"
-function Messages(props){
+import { React, useState } from "react"
+function Messages(props) {
     const toggleNewDM = props.newDM
     const messagePeople = [{
         username: "Zaraki",
@@ -15,36 +15,40 @@ function Messages(props){
         username: "Barbie",
     }]
     const [users, setUsers] = useState(messagePeople)
-    const setMessages = () =>{
+    const setMessages = () => {
         setUsers(messagePeople)
     }
-    const setRequests = () =>{
+    const setRequests = () => {
         setUsers(requestPeople)
     }
-    return(
-        <div>
-            <button onClick={setMessages}> Messages</button>
-            <button onClick={setRequests}> Requests</button>
-            <button onClick={toggleNewDM}> New DM</button>
-            {users.map((person,index) =>(
-                <div>{person.username}</div>
-            ))}
+    return (
+        <div class='msg-buttons'>
+            <input type="text" placeholder="Search.."></input>
+            <button id='msg_btn' onClick={setMessages}> Messages</button>
+            <button id='req_btn' onClick={setRequests}> Requests</button>
+            <button id='newDm_btn' onClick={toggleNewDM}> New DM</button>
+
+            <div class = 'users_idf'>
+            {users.map((person, index) => (
+                    <div>{person.username}</div>
+                ))}
+            </div>
         </div>
     )
 }
-function NewMessage(props){
+function NewMessage(props) {
     const toggleDefault = props.back
     const toggleDM = props.displayDM
-    return(<div>
+    return (<div>
         List of people you can send a new dm to
         <h1>Another map function like I did above</h1>
         <button onClick={toggleDefault}> Go back to messages and requests</button>
         <button onClick={toggleDM}> See DM with Zaraki</button>
     </div>)
 }
-function Messaging(props){
+function Messaging(props) {
     const back = props.back
-    return(
+    return (
         <div>
             <div> DM with Zaraki</div>
             <button onClick={back}> Go back</button>
@@ -52,24 +56,28 @@ function Messaging(props){
     )
 }
 function DMOverLay(props) {
+    const toggleNewDM = props.newDM
     const closePopup = props.closeEvent
-    const [display, setDisplay] = useState(<Messages newDM ={showNewMessage}/>)
-    function showMessages(){
-        setDisplay(<Messages newDM ={showNewMessage}/>)
+    const [display, setDisplay] = useState(<Messages newDM={showNewMessage} />)
+    function showMessages() {
+        setDisplay(<Messages newDM={showNewMessage} />)
     }
-    function showNewMessage(){
-        setDisplay(<NewMessage displayDM ={showMessaging} back={showMessages}/>)
+    function showNewMessage() {
+        setDisplay(<NewMessage displayDM={showMessaging} back={showMessages} />)
     }
-    function showMessaging(){
-        setDisplay(<Messaging back = {showNewMessage}/>)
+    function showMessaging() {
+        setDisplay(<Messaging back={showNewMessage} />)
     }
+
     return (
         <div id='dm-overlay'>
-            
-            <button onClick={closePopup}>Close The whole thing</button>
+            <button id='newDm_btn' onClick={toggleNewDM}> New DM</button>
+            <p>Inbox</p>
+            <button id = 'close_btn' onClick={closePopup}></button>
+
             {display}
         </div>
-  );
+    );
 }
 
 export default DMOverLay;
