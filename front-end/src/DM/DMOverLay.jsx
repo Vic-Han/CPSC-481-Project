bb07a8db7ac9118d1729fd6d4d99c8107c7ec635
MohammedH41 <122569808+MohammedH41@users.noreply.github.com>
2023-11-23 15:08:52 -0700

    minor changes

--- front-end/src/DM/DMOverLay.jsx
+++ front-end/src/DM/DMOverLay.jsx
@@ -9,11 +9,10 @@ import user5 from '../assets/Person5.png'
 import user6 from '../assets/Person6.png'
 import user7 from '../assets/Person7.png'
 import user8 from '../assets/Person8.png'
-
-
 import basicpfp from '../assets/basicpfp.png'
 
 
+//function for displaying Inbox items 
 function Messages(props) {
     const toggleNewDM = props.newDM
     const closePopup = props.closePopup
@@ -75,68 +74,8 @@ function Messages(props) {
         </>
     )
 }
-function NewMessage(props) {
-    const toggleDefault = props.back
-    const toggleDM = props.displayDM
-    return (<div>
 
-        <header className='msg_header'>
-            <h1 id='newMsgheader'>New Message</h1>
-            <button id='back_btn' onClick={toggleDefault}> </button>
-        </header>
-
-        <div className='msg_type'>
-            <input id = 'txt_field' type="text" placeholder="Search"></input>
-        </div>
-
-        <div onClick={toggleDM}>
-            <UserList users={userListData} />
-        </div>
-
-    </div>)
-}
-function Messaging(props) {
-    const back = props.back
-    const [messages, setMessages] = useState([]);
-    const [newMessage, setNewMessage] = useState('');
-  
-    const handleSendMessage = () => {
-      if (newMessage.trim() !== '') {
-        setMessages([...messages, { text: newMessage, sender: 'user' }]);
-        setNewMessage('');
-      }
-    };
-
-    return (
-        <div>
-            <header id='msg_header2'>
-                <img id='kenpfp' src={Kenpachipfp} alt="Profile" />
-                <p id='inbox_style'>Kenpachi</p>
-                <button id='back_btn' onClick={back}></button>   
-            </header>
-
-            <div className="chat-container">
-                <div className="chat-messages">
-                    {messages.map((message, index) => (
-                    <div key={index} className={message.sender === 'user' ? 'user-message' : 'other-message'}>
-                        {message.text}
-                    </div>
-                    ))}
-                </div>
-                <div className="chat-input">
-                    <input
-                    type="text"
-                    placeholder="Type your message..."
-                    value={newMessage}
-                    onChange={(e) => setNewMessage(e.target.value)}
-                    />
-                    <button id = 'send_msg' onClick={handleSendMessage}></button>
-                </div>
-            </div>
-        </div>
-    )
-}
-
+//fucntion for displaying pfp newMessages 
 function UserList({ users }) {
     const renderUserList = () => {
       return users.map((user, index) => (
@@ -186,6 +125,76 @@ function UserList({ users }) {
 
   ];
   
+//function for displaying NewMessage items 
+function NewMessage(props) {
+    const toggleDefault = props.back
+    const toggleDM = props.displayDM
+    return (<div>
+
+        <header className='msg_header'>
+            <h1 id='newMsgheader'>New Message</h1>
+            <button id='back_btn' onClick={toggleDefault}> </button>
+        </header>
+
+        <div className='msg_type'>
+            <input id = 'txt_field' type="text" placeholder="Search"></input>
+        </div>
+
+        <div onClick={toggleDM}>
+            <UserList users={userListData} />
+        </div>
+
+    </div>)
+}
+function Messaging(props) {
+    const back = props.back
+    const [messages, setMessages] = useState([]);
+    const [newMessage, setNewMessage] = useState('');
+  
+    const handleSendMessage = () => {
+      if (newMessage.trim() !== '') {
+        setMessages([...messages, { text: newMessage, sender: 'user' }]);
+        setNewMessage('');
+      }
+    };
+
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+          handleSendMessage();
+        }
+      };
+
+    return (
+        <div>
+            <header id='msg_header2'>
+                <img id='kenpfp' src={Kenpachipfp} alt="Profile" />
+                <p id='inbox_style'>Zaraki Kenpachi</p>
+                <button id='back_btn' onClick={back}></button>   
+            </header>
+
+            <div className="chat-container">
+                <div className="chat-messages">
+                    {messages.map((message, index) => (
+                    <div key={index} className={message.sender === 'user' ? 'user-message' : 'other-message'}>
+                        {message.text}
+                    </div>
+                    ))}
+                </div>
+                <div className="chat-input">
+                    <input
+                    type="text"
+                    placeholder="Type your message..."
+                    value={newMessage}
+                    onKeyPress={handleKeyPress}
+                    onChange={(e) => setNewMessage(e.target.value )}
+                    />
+                    <button id = 'send_msg' onClick={handleSendMessage}></button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
 
 
 function DMOverLay(props) {
