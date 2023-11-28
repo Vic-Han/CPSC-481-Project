f4bb12e722ee45ede5a9890d30ced142af182f6c
MohammedH41 <122569808+MohammedH41@users.noreply.github.com>
2023-11-27 21:57:43 -0700

    minor styling to MyProfile page

--- front-end/src/Profile/MyProfile.jsx
+++ front-end/src/Profile/MyProfile.jsx
@@ -2,6 +2,8 @@ import {React, useState, useEffect} from "react"
 import './MyProfile.css'
 import FollowerOverLay from "./FollowerOverLay"
 import FollowingOverLay from "./FollowingOverLay"
+import user from "../assets/Person9.png"
+
 function MyProfile(props){
     const [follower, setFollower] = useState(false)
     const [following, setFollowing] = useState(false)
@@ -11,11 +13,26 @@ function MyProfile(props){
     const hideFollowing = () =>{ setFollowing(false)}
     return(
       <>
-      <div>My Profile</div>
-      <button onClick={showFollower}> See followers list</button>
-      <button onClick={showFollowing}> See following list</button>
-      {follower ? <FollowerOverLay close ={hideFollower} viewSelf = {true}/>: null}
-      {following ? <FollowingOverLay close ={hideFollowing} viewSelf = {true}/>: null}
+
+      <div className="profile_container">
+        <img id ="myProfile_pfp" src= {user} alt="profile pic" />
+        <div id = "myProfile_dp" >Ken Barbie</div>      
+        <div id = "myProfile_username" >Ken4Lyfe</div>
+        <p id="myProfile_bio" >I am a professional ESports Enjoyer It’s awesome.</p>
+
+        <div className="myPofile_follow">
+          <button onClick={showFollower}> See followers list</button>
+          <button onClick={showFollowing}> See following list</button>
+          {follower ? <FollowerOverLay close ={hideFollower} viewSelf = {true}/>: null}
+          {following ? <FollowingOverLay close ={hideFollowing} viewSelf = {true}/>: null}  
+        </div>
+
+        <button id="myProfile_logout">logout</button>
+        <button id="setting_button" ></button>
+        <button id="share_button" ></button>
+
+      </div>
+
       </>
     )
 }
