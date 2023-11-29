e6f889f2f89929d6ec152998b8131f1a9546fff2
MohammedH41 <122569808+MohammedH41@users.noreply.github.com>
2023-11-29 14:38:38 -0700

    Recent tag list created in myProfile page

--- front-end/src/Profile/MyProfile.jsx
+++ front-end/src/Profile/MyProfile.jsx
@@ -17,15 +17,24 @@ function MyProfile(props){
       Date: 'November 28, 2023',
       Tag: 'E-Sports Event',
       Tag2: 'asdasd',
-      Discrition: 'Just the Title, why is ESports Dying?!?!?!? I Enjoy ESports and I don’t want it to die off, it’s like the only thing keeping me interested in the gaming industry and if it dies off I won’t be able to cheer for my favorite team. I don’t watch sports because I am not too interested in sports, but I am interested in gaming, why must ESports be dying.'
+      Discription: 'Just the Title, why is ESports Dying?!?!?!? I Enjoy ESports and I don’t want it to die off, it’s like the only thing keeping me interested in the gaming industry and if it dies off I won’t be able to cheer for my favorite team. I don’t watch sports because I am not too interested in sports, but I am interested in gaming, why must ESports be dying.'
     }
     const post2 = {
       Title: 'Upcoming Valorant ESports Event Locations?',
       Date: 'October 26, 2023',
       Tag: ['Valorant', 'asdsa'],
-      Discrition: 'Just the Title, why is ESports Dying?!?!?!? I Enjoy ESports and I don’t want it to die off, it’s like the only thing keeping me interested in the gaming industry and if it dies off I won’t be able to cheer for my favorite team. I don’t watch sports because I am not too interested in sports, but I am interested in gaming, why must ESports be dying.'
+      Discription: 'Looking for the inside scoop on upcoming Valorant esports event locations? Dive into the action-packed world of Valorant and discover where the hottest competitions are set to take place! With Valorants esports scene heating up, fans and players are eager to know where the next big showdowns will be. Stay in the loop and find out the exciting cities and venues that will host the thrilling Valorant tournaments, creating an electric atmosphere for both players and viewers alike.'
+    }
+    const post3 = {
+      Title: 'need to make more uniqe posts',
+      Date: 'October 26, 2023',
+      Tag: ['Valorant'],
+      Discription: 'Just the Title, why is ESports Dying?!?!?!? I Enjoy ESports and I don’t want it to die off, it’s like the only thing keeping me interested in the gaming industry and if it dies off I won’t be able to cheer for my favorite team. I don’t watch sports because I am not too interested in sports, but I am interested in gaming, why must ESports be dying.'
     }
 
+    const tagList = {
+      tags: ['asdasd', 'asdasd', 'asdasda', 'asdasdassss']
+    }
 
     {/* function to create posts */}
     const createPost = (post) => (
@@ -45,7 +54,7 @@ function MyProfile(props){
             </ul>
           </div>
           <div className='line'></div>
-          <p className="preview_text"> {post.Discrition}</p>
+          <p className="preview_text"> {post.Discription}</p>
         </div>
       </>
     );
@@ -54,6 +63,24 @@ function MyProfile(props){
       return (
         <div className="myProfile_post">
           {createPost(post)}
+        </div>
+      );
+    }
+
+    const TagList = ({ tagList }) => {
+      const { tags } = tagList;
+    
+      return (
+        <div className='tag-list-container'>
+          {tags && tags.length > 0 ? (
+            <div className='tag-list'>
+              {tags.map((tag, index) => (
+                <div key={index} className='tag'>{tag}</div>
+              ))}
+            </div>
+          ) : (
+            <p>No tags available.</p>
+          )}
         </div>
       );
     }
@@ -68,8 +95,8 @@ function MyProfile(props){
         <div id="myProfile_username">Ken4Lyfe</div>
         <p id="myProfile_bio">I am a professional ESports Enjoyer. It's awesome.</p>
         <div className="myPofile_follow">
-          <button onClick={showFollower}>Followers</button>
-          <button onClick={showFollowing}>Following</button>
+          <button onClick={showFollower}>Followers <br />102</button>
+          <button onClick={showFollowing}>Following<br />121</button>
           {follower ? <FollowerOverLay close={hideFollower} viewSelf={true} /> : null}
           {following ? <FollowingOverLay close={hideFollowing} viewSelf={true} /> : null}
         </div>
@@ -85,14 +112,22 @@ function MyProfile(props){
       </div>
     </div>
   </div>
-    
 
+  <div className="myProfile_tagList">
+      <p >Recent Tag</p>
+      <div>
+        <TagList tagList= {tagList}/>
+      </div>
+  </div>
 
+  <div id="myProfile_post_header">
+    <p>Posts (14)</p>
+  </div>
 
   <div className="myProfile_post">
-  <PostComponent post={post1} />
-  <PostComponent post={post2} />
-
+    <PostComponent post={post1} />
+    <PostComponent post={post2} />
+    <PostComponent post={post3} />
   </div>
 
 </>
